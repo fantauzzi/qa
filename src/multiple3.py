7f9e3fa45811ae17dd7b6b184af0484469e592aa
fantauzzi <Francesco_Fantauzzi@yahoo.com>
2023-07-04 15:10:49 +0200

    Fixed parallel download to play nice by arxiv requirements

--- src/multiple3.py
+++ src/multiple3.py
@@ -1,8 +1,7 @@
 import asyncio
-from time import sleep
+from time import sleep, time
 
 import aiohttp
-# import backoff
 
 chunk_size = 1024 * 1024
 concurrent_reqs = 4
@@ -12,12 +11,11 @@ class DownloadFailed(Exception):
     pass
 
 
-# @backoff.on_exception(backoff.expo, Exception)
 async def download_file(session, url):
     async with session.get(url) as response:
         if response.status == 200:
             filename = url.split('/')[-1]
-            with open(f'../dataset/download/{filename}.pdf', 'wb') as f:
+            with open(f'/home/fanta/Downloads/arxiv/{filename}.pdf', 'wb') as f:
                 while True:
                     chunk = await response.content.read(chunk_size)
                     if not chunk:
@@ -42,10 +40,22 @@ async def download_files(urls):
 
 
 async def main():
-    urls_file = "../dataset/files2.txt"  # Path to the text file containing URLs
+    urls_file = "../dataset/files.txt"  # Path to the text file containing URLs
     with open(urls_file, 'r') as f:
         urls = f.read().splitlines()
-        await download_files(urls)
+    idx = 0
+    start_time = time()
+    while idx < len(urls):
+        batch = urls[idx: idx + concurrent_reqs]
+        await download_files(batch)
+        completed = min(len(urls), idx+concurrent_reqs)
+        current_time = time()
+        elapsed = current_time - start_time
+        rate = completed / elapsed
+        estimate_time_left = (len(urls) - completed)/rate
+        print(f'Downloaded {completed} files in {int(elapsed)} sec, estimated time left {int(estimate_time_left)} sec, {estimate_time_left/3600} hours')
+        sleep(1.1)
+        idx+=concurrent_reqs
 
 
 if __name__ == "__main__":
